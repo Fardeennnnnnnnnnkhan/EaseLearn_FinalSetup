46a12d1938e6ba63000c949c91261b1c3a5e9936
samarJ19 <samarjoshitech@gmail.com>
2025-03-08 01:15:25 +0530

    minor change

--- frontend/eLearning/src/main.jsx
+++ frontend/eLearning/src/main.jsx
@@ -4,8 +4,9 @@ import App from './App.jsx'
 import { UserContextProvider } from './context/UserContext.jsx'
 import { CourseContextProvider } from './context/CourseContext.jsx'
 
-export const server = 'http://localhost:3000'
+//export const server = 'http://localhost:3000'
 // export const server = 'https://ease-learn-final-setup.vercel.app'
+export const server ='https://easelearn-byfardeen.onrender.com'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
